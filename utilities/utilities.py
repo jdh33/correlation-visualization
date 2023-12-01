ba7f6ab03129375d07d6dcf758287b141decfe07
Jonah Hocum <jonahdhocum@gmail.com>
2023-12-01 15:21:19 -0800

    Implement Variance inflation factor function Implement compute_vif in utilities.py

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -1,5 +1,41 @@
 import numpy as np
 import argparse
+import pandas as pd
+from statsmodels.stats.outliers_influence import (
+    variance_inflation_factor as vif)
+
+
+""" Data cleanup utilities """
+def fillna_by_number_dtype(df):
+    """
+    This is handles the WHO Life Expectancy data specifically. 
+    It could definitely be generalized see if a dtype is any 
+    type of int, etc.
+    """
+    for col in df:
+        data_type = df[col].dtype
+        if data_type == np.int64:
+            df[col] = df[col].fillna(
+                df[col].median().round(0).astype(np.int64))
+        elif data_type == np.float64:
+            df[col] = df[col].fillna(df[col].median())
+
+
+""" Data exploration utilities """
+# Modifed from: https://towardsdatascience.com/
+# targeting-multicollinearity-with-python-3bd3b4088d0b
+# Compute the vif for all given features
+def compute_vif(df, considered_features):
+    X = df.reindex(considered_features, axis=1)
+    fillna_by_number_dtype(X)
+    # The calculation of variance inflation requires a constant
+    X['intercept'] = 1
+    # Create dataframe to store vif values
+    df_vif = pd.DataFrame()
+    df_vif["Variable"] = X.columns
+    df_vif["VIF"] = [vif(X.values, i) for i in range(X.shape[1])]
+    df_vif = df_vif[df_vif['Variable']!='intercept']
+    return df_vif
 
 """ Model training utilities """
 def get_model_training_args():
@@ -30,18 +66,3 @@ def get_model_training_args():
                         help='Number of threads to use during training.')
     args = parser.parse_args()
     return args
-
-
-""" Data cleanup utilities """
-def fillna_by_number_dtype(df):
-    """
-    This is handles the WHO Life Expectancy data specifically. 
-    It could definitely be generalized see if a dtype is any 
-    type of int, etc.
-    """
-    for col in df:
-        data_type = df[col].dtype
-        if data_type == np.int64:
-            df[col] = df[col].fillna(df[col].median().round(0).astype(np.int64))
-        elif data_type == np.float64:
-            df[col] = df[col].fillna(df[col].median())
