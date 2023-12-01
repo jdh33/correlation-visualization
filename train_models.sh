25a21258624d9c9ced8e4b9a01ae9e2a21082ae6
Jonah Hocum <jonahdhocum@gmail.com>
2023-12-01 15:15:36 -0800

    Update train_models.sh Modify passed arguments to reflect dataset name change

--- train_models.sh
+++ train_models.sh
@@ -4,10 +4,6 @@
 # OUTPUT_SUFFIX=`date +%Y%m%d`
 N_JOBS=1
 
-python scripts/train_models.py --training_dataset "who_life_expectancy" \
+python scripts/train_models.py --training_dataset "life-expectancy-who" \
         --target_variable "Life expectancy" --train_with_covar --train_with_shuffled_data \
         --timestamp --n_jobs=$N_JOBS
-
-# python scripts/train_models.py --training_dataset "who_life_expectancy" \
-#         --target_variable "Life expectancy" --selected_models "$SELECTED_MODELS" \
-#         --train_with_covar --train_with_shuffled_data --timestamp --n_jobs=$N_JOBS
